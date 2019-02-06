decd4f3f36fcfc27d2f122fdef4eb45603547800
procodez <44534433+procodez@users.noreply.github.com>
2019-02-05 19:48:20 -0600

    Add files via upload

--- harmless1.py
+++ harmless1.py
@@ -1,5 +1,15 @@
 import webbrowser
+import tkinter
+from tkinter import messagebox
+
+root = tkinter.Tk()
+root.withdraw()
+messagebox.showinfo("Wack", "Neato")
+messagebox.showwarning("Bad", "Whoops")
+messagebox.showerror("Oopsie", "Something will happen")
+
 i = 0
-while i <= 10:
+while i <= 30:
     webbrowser.open_new_tab("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     i += 1
+    print("Congrats, you played yourself")
