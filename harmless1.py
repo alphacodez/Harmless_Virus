d262812c325f5d70418b1e1c6604394a1d82cd1e
procodez <44534433+procodez@users.noreply.github.com>
2019-02-06 18:11:01 -0600

    Update harmless1.py

--- harmless1.py
+++ harmless1.py
@@ -9,7 +9,7 @@ messagebox.showwarning("Bad", "Whoops")
 messagebox.showerror("Oopsie", "Something will happen")
 
 i = 0
-while i <= 30:
+while i <= 50:
     webbrowser.open_new_tab("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     i += 1
     print("Congrats, you played yourself")
